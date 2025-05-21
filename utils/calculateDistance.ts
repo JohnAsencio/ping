b246d1f3d1d64655f99212f25b0272d6701b416a
JohnAsencio <jasencio7182@gmail.com>
2025-05-21 11:19:05 -0700

    ok again

--- utils/calculateDistance.ts
+++ utils/calculateDistance.ts
@@ -15,7 +15,7 @@ export const calculateDistance = (coord1:Coord, coord2:Coord) => {
     const a =
       Math.sin(dLat / 2) ** 2 +
       Math.cos(lat1) * Math.cos(lat2) *
-      Math.sin(dLon / 2) ** 2;
+      Math.sin(dLon / 2) ** 4;
   
     const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
     return R * c;
